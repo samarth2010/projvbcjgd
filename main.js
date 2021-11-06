2f6a272bcf664bb381f6b59ddcccfb7e3cb0aca8
samarth2010 <76905546+samarth2010@users.noreply.github.com>
2021-11-06 17:21:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function bed() {
+  window.location="bd.html"
+  
+}
+function tv() {
+  window.location="tv.html"
+  
+}
+function botts() {
+  window.location="botts.html"
+  
+}
+function lptp() {
+  window.location="lptp.html"
+  
+}
+function sofa() {
+  window.location="sofa.html"
+  
+}
